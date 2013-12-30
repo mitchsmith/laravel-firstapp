5450e234dd8863b0d25c3d2f97289e3dd12e8c54
Mitch Smith <mitch_smith@wgbh.org>
2013-12-29 22:44:09 -0600

    Fixed formatting issue

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -1,11 +1,13 @@
 @extends('layout')
 
+@section('content')
+
 <div>
 	<h1>List of Users</h1>
 	<p></p>
-@section('content')
     @foreach($users as $user)
         <p>{{ $user->name }}</p>
     @endforeach
+
 </div>
 @stop
